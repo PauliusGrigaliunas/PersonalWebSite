d1193c4ce82154b774ed9382376f3ca931508a88
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-07-15 14:35:11 +0300

    dropDown list 1

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Route, Link } from "react-router-dom";
-import Dropdown from "react-dropdown";
 import search from "../../images/search.svg";
 import bell from "../../images/bell.svg";
 import caretdown from "../../images/caret-down.svg";
 import foto from "../../images/paulius.png";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+var DropdownTrigger = Dropdown.DropdownTrigger;
+var DropdownContent = Dropdown.DropdownContent;
 
 export default function Header() {
+  const options = ["one", "two", "three"];
   return (
     <div className="container__header">
       <header className="header">
@@ -33,6 +38,33 @@ export default function Header() {
             <div className="header__profile-name">Paulius Grigaliūnas</div>
             <img src={caretdown} alt="caret-down" />
           </Link>
+          <Dropdown options={options}>
+            <div class="header__profile-nav">
+              <img
+                src={foto}
+                className="photo header__profile-photo"
+                alt="placeholder"
+              />
+              <div className="header__profile-name">Paulius Grigaliūnas</div>
+              <img src={caretdown} alt="caret-down" />
+            </div>
+            >
+            <DropdownTrigger />
+            <DropdownContent>
+              <img src={foto} alt="me" /> Username
+              <ul>
+                <li>
+                  <a href="/profile">Profile</a>
+                </li>
+                <li>
+                  <a href="/favorites">Favorites</a>
+                </li>
+                <li>
+                  <a href="/logout">Log Out</a>
+                </li>
+              </ul>
+            </DropdownContent>
+          </Dropdown>
         </div>
       </header>
     </div>
