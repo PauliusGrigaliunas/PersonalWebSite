d890d9640e82479f76c20cacb394386d049d786c
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-07-17 22:31:29 +0300

    something about routers

--- src/components/main/help/HelpTabs.jsx
+++ src/components/main/help/HelpTabs.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { BrowserRouter as Route, Link } from "react-router-dom";
+import Help from "./Help";
+import Contacts from "./Contacts";
+import Comments from "./Comments";
 
 export default function HelpTabs() {
   return (
-    <nav>
-      <ul className="tabs content__fixed">
-        <li>
-          <Link to="/help">
-            <div className="tabs__link">Bendra informacija</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/contacts">
-            <div className="tabs__link">Kontaktai</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/comments">
-            <div className="tabs__link">Komentarai</div>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <React.Component>
+      <nav>
+        <ul className="tabs content__fixed">
+          <li>
+            <Link to="/help">
+              <div className="tabs__link">Bendra informacija</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/contacts">
+              <div className="tabs__link">Kontaktai</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/comments">
+              <div className="tabs__link">Komentarai</div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/help" component={Help} />
+      <Route path="/contacts" component={Contacts} />
+      <Route path="/comments" component={Comments} />
+    </React.Component>
   );
 }
