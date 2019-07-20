2479bf133f8cf2241fc8908fba4d743d7b42d4d1
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-07-20 15:11:25 +0300

    Save

--- src/components/main/news/NewsContext.jsx
+++ src/components/main/news/NewsContext.jsx
@@ -5,14 +5,9 @@ export const NewsContext = createContext();
 export const NewsProvider = props => {
   const [news, setNews] = useState([
     {
-      title: "Pradžia",
-      text: "Čia yra pirmasis testinis tekstas",
+      title: "Pavyzdys",
+      text: "Čia yra testinis tekstas",
       id: 1
-    },
-    {
-      title: "Antroji ivestis",
-      text: "Čia yra antrasis testinis tekstas",
-      id: 2
     }
   ]);
   return (
